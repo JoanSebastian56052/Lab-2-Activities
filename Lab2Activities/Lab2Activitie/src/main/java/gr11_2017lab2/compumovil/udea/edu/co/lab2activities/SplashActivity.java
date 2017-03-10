4f303d194afb2caeb2028bbc92ff01fa64a0f792
joansebastian56052 <chicoriolds@gmail.com>
2017-03-09 19:18:25 -0500

    Creacion de clases vacias

---
+++ Lab2Activities/Lab2Activitie/src/main/java/gr11_2017lab2/compumovil/udea/edu/co/lab2activities/SplashActivity.java
@@ -0,0 +1,13 @@
+package gr11_2017lab2.compumovil.udea.edu.co.lab2activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+    }
+}
