0c9f318f54630e35212e30be2e3f0b0a28695e24
joansebastian56052 <chicoriolds@gmail.com>
2017-03-11 16:25:12 -0500

    Finalizacion clases de logueo y registro,corregir no logueo, restates -> clase NavigationDrawer

--- Lab2Activities/Lab2Activitie/src/main/java/gr11_2017lab2/compumovil/udea/edu/co/lab2activities/SplashActivity.java
+++ Lab2Activities/Lab2Activitie/src/main/java/gr11_2017lab2/compumovil/udea/edu/co/lab2activities/SplashActivity.java
@@ -1,13 +1,37 @@
 package gr11_2017lab2.compumovil.udea.edu.co.lab2activities;
 
-import android.support.v7.app.AppCompatActivity;
+
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 
-public class SplashActivity extends AppCompatActivity {
+public class SplashActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        // TODO Auto-generated method stub
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+        Thread timerThread = new Thread(){
+            public void run(){
+                try{
+                    sleep(1500);
+                }catch(InterruptedException e){
+                    e.printStackTrace();
+                }finally{
+                    Intent intent = new Intent(SplashActivity.this,LoginActivity.class);
+                    startActivity(intent);
+                }
+            }
+        };
+        timerThread.start();
     }
+    @Override
+    protected void onPause() {
+        // TODO Auto-generated method stub
+        super.onPause();
+        finish();
+    }
+
 }
